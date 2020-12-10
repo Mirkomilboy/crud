326d1546cf15eb99ae822c69eba33afcfce00bb5
Mirkomilboy Sadirov <msadirov1997@gmail.com>
2020-12-10 19:04:01 +0500

    CRUD application

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <!-- requiring process.php -->
+    <?php require_once 'process.php'; ?>
+
+    <div class="container mt-3">
+        <!-- Session message -->
+        <?php
+            if(isset($_SESSION['message'])): ?>
+            <div class="alert alert-<?=$_SESSION['msg_type']?>">
+                <?php
+                    echo $_SESSION['message'];
+                    unset($_SESSION['message']);
+                ?>
+            </div>
+        <?php endif ?>
+        <?php
+            $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+            $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+            ?>
+
+            <div class="row justify-content-center">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th colspan="2">Action</th>
+                        </tr>
+                    </thead>
+                    <?php while($row = $result->fetch_assoc()): ?>
+                    <tr>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['location']; ?></td>
+                        <td>
+                            <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
+                            <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+                </table>
+            </div>
+        <div class="row justify-content-center">
+            <form action="process.php" method="post">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <div class="form-group">
+                    <label for="">Name</label>
+                    <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Enter your name">
+                </div>
+                <div class="form-group">
+                    <label for="">Location</label>
+                    <input type="text" name="location" class="form-control" value="<?php echo $location; ?>" placeholder="Enter your location">
+                </div>
+                <div class="form-group">
+                    <?php
+                        if($update == true): ?>
+                        <button type="submit" class="btn btn-info" name="update">Update</button>
+                        <?php else: ?>
+                    <button type="submit" class="btn btn-primary" name="save">Save</button>
+                    <?php endif; ?>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
